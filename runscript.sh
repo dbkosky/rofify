95d67757151aa74e1ee5746b9418d5ec8ecac075
Daniel Kosky <dbkosky@gmail.com>
2021-04-26 00:36:28 +0100

    change runscript to use correct paths for the theme and config files, etc

--- runscript.sh
+++ runscript.sh
@@ -3,8 +3,9 @@
 # Export relevant flags used by python as environment variables
 # Negative width is rofi estimating character width, 
 # e.g. -90 is roughly 90 characters width
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export WIDTH=-90
-rofi -modi mymenu:$PWD/rofify/__main__.py\
-	-show mymenu -theme rofify_theme.rasi\
-	-config rofify.config -width $WIDTH\
+rofi -modi mymenu:$DIR/rofify/__main__.py\
+	-show mymenu -theme $DIR/rofify_theme.rasi\
+	-config $DIR/rofify.config -width $WIDTH\
 	-show-icons
