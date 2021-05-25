2f90491d0a9b8cb9b0d4cb3f0e52ae4c23c992c9
Daniel Kosky <dbkosky@gmail.com>
2021-05-25 20:45:26 +0100

    hotkeys in device menu

--- rofify/src/DeviceMenu.py
+++ rofify/src/DeviceMenu.py
@@ -1,13 +1,12 @@
 from rofify.src.SpotifyAPI import spotify
+from rofify.src.Hotkeys import hotkeys
 from rofify.src.config import config
-from rofi_menu import Menu, Item, BackItem, Operation, constants
+import rofi_menu
 import asyncio
-
-import sys
 
 active_label = lambda x: "<span foreground='{}'>".format(config.get_format('active-item-colour')) + str(x) + " (active)</span>"
 
-class DeviceItem(Item):
+class DeviceItem(rofi_menu.Item):
     """
     Representing a device in the device menu. Should display the item differently 
     if the device is active, and allow the user to transfer playback to the related
@@ -35,7 +34,7 @@ class DeviceItem(Item):
         return await super().on_select(meta)
 
 
-class DeviceMenu(Menu):
+class DeviceMenu(rofi_menu.Menu):
     """
     Menu accessible from the top menu that allows user to see the available devices and 
     transfer playback between them
@@ -51,7 +50,7 @@ class DeviceMenu(Menu):
     # TODO add a method to allow user to favourite devices and cache device id
     async def generate_menu_items(self, meta):
 
-        items = [BackItem()] 
+        items = [rofi_menu.BackItem()]
 
         # This is used to keep track of which device is active between user input
         meta.session.setdefault('active_device', None)
@@ -64,3 +63,9 @@ class DeviceMenu(Menu):
             items.append(DeviceItem(device=device))
         
         return items
+
+    async def on_user_input(self, meta):
+
+        await hotkeys.handle_user_input()
+
+        return rofi_menu.Operation(rofi_menu.constants.OP_REFRESH_MENU)
